3c02df34244ece01e0f40377789cdce8e2d085f8
wjkim <wnkwj66@gmail.com>
2024-10-01 15:07:45 +0900

    Spring Data JPA 활용

--- src/main/java/hello/hollo_spring/SpringConfig.java
+++ src/main/java/hello/hollo_spring/SpringConfig.java
@@ -12,36 +12,57 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-
-//    @PersistenceContext
-    private EntityManager em;
-
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
-    /*
+/*
+    // DB 직접 연동
     private DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-    */
+*/
+/*
+    // JPA 설정
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
+ */
 
     /**
      * 자바 코드로 직접 스프링빈 등록
      */
+/*
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-
+*/
+/*
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+//        return new JpaMemberRepository(em);
+
+    }
+
+ */
+
+    // SpringData JPA
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
 }
